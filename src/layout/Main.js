a67d5d0764b39dfc8bd731cfc4facd9619f8ae87
07arif <ai.arif0711@gmail.com>
2022-12-25 00:35:25 +0600

    layout setup done

---
+++ src/layout/Main.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from '../pages/Shared/Header/Header';
+import LeftSideNav from '../pages/Shared/LeftSideNav/LeftSideNav';
+import RightSideNav from '../pages/Shared/RightSideNav/RightSideNav';
+import Footer from '../pages/Shared/Footer/Footer'
+
+const Main = () => {
+    return (
+       <div>
+        <Header/>
+         <Container>
+            <Row>
+                <Col lg="2">
+                    <LeftSideNav/>
+                </Col>
+                <Col lg="7">
+                    <Outlet></Outlet>
+                </Col>
+                <Col lg="3">
+                    <RightSideNav/>
+                </Col>
+
+            </Row>
+        </Container>
+        <Footer></Footer>
+       </div>
+    );
+};
+
+export default Main;
